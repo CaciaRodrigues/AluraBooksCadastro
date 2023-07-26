854cd59c1ef435130a4d3d6dac47b75267049728
CaciaRodrigues <caciarodriguues@gmail.com>
2023-07-26 18:23:23 -0300

    Terminando ou auto-complete e mensagem de erro caso CEP inválido

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
+    let mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
-    var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/');
-    var consultaCEPConvertida = await consultaCEP.json();
+    let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    let consultaCEPConvertida = await consultaCEP.json();
     if (consultaCEPConvertida.erro) {
         throw Error('CEP não existente!');
     }
+    let endereco = document.getElementById('endereco');
+    let bairro = document.getElementById('bairro');
+    let cidade = document.getElementById('cidade');
+    let estado = document.getElementById('estado');
+
+    endereco.value = consultaCEPConvertida.logradouro;
+    bairro.value = consultaCEPConvertida.bairro;
+    cidade.value = consultaCEPConvertida.localidade;
+    estado.value = consultaCEPConvertida.uf;
+
     console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
 } catch (erro) {
+    mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
     console.log(erro);
 }
 }
 
-buscaEndereco();
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
