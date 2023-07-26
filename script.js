21cbfc619c4aea9d5387a51a79e136b48decde7f
CaciaRodrigues <caciarodriguues@gmail.com>
2023-07-26 17:33:36 -0300

    Aprendendo async await e try catch

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if(r.erro) {
-        throw Error('Esse CEP não existe!')
-    }else console.log(r)})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluído!'));
+async function buscaEndereco() {
+    try {
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/');
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+        throw Error('CEP não existente!');
+    }
+    console.log(consultaCEPConvertida);
+} catch (erro) {
+    console.log(erro);
+}
+}
 
-console.log(consultaCEP);
+buscaEndereco();
